a026f447ab4db8d9f142e03b2c60052c77cb6a3b
shogo0625 <shogo19940625@gmail.com>
2020-03-04 07:18:36 +0000

    [Finish] 課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,29 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,:validatable
 
+  attachment :profile_image, destroy: false
+
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  attachment :profile_image, destroy: false
 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following_user.include?(user)
+  end
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,19 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about' => 'home#about'
   devise_for :users
+
+  devise_scope :user do
+ 		get 'users/:id/follows' => 'users#follows', as: 'follow_user'
+ 		get 'users/:id/followers' => 'users#followers', as: 'follower_user'
+ 	end
+
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
   	resource :favorites, only:[:create, :destroy]
   	resource :book_comments, only:[:create, :destroy]
   end
+
+  post 'follow/:id' => 'relationships#follow', as:'follow'
+  delete 'unfollow/:id' => 'relationships#unfollow', as:'unfollow'
+
 end
